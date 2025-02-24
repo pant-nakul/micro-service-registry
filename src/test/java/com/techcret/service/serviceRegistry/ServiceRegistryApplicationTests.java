b4a8045fb456082a5df79126102c03492de01e95
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-02-24 23:41:36 +0530

    Initial commit service registry

---
+++ src/test/java/com/techcret/service/serviceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcret.service.serviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
